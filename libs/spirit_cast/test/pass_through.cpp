7a9fac8152fa7c8ffdd5e08483a899d71b9c88da
Jeroen Habraken <vexocide@gmail.com>
2010-07-10 15:57:13 +0200

    Added pass-through tests

---
+++ libs/spirit_cast/test/pass_through.cpp
@@ -0,0 +1,18 @@
+//              Copyright Jeroen Habraken 2010.
+// 
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file ../../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#define BOOST_TEST_MODULE pass_through
+
+#include <boost/test/unit_test.hpp>
+#include <boost/spirit_cast.hpp>
+
+BOOST_AUTO_TEST_CASE(pass_through) {
+    BOOST_CHECK(boost::spirit_cast<int>(1.0) == 1);
+}
+
+BOOST_AUTO_TEST_CASE(pass_through_downcast) {
+    BOOST_CHECK(boost::spirit_cast<double>(1) == 1.0);
+}
