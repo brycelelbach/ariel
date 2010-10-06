9d7407d87709ebebedbbb2bc1808cb9d827d1614
Bryce Lelbach <admin@thefireflyproject.us>
2010-10-06 16:05:08 -0400

    Regenned all tests in svg, modified the nt2 set vs mpl set tests.

--- test/profiler/nt2_vs_mpl/mpl_has_key.cxx
+++ test/profiler/nt2_vs_mpl/mpl_has_key.cxx
@@ -1,14 +1,31 @@
-#include <cstdio>
-#include <boost/mpl/set.hpp>
+#include <boost/mpl/set/set10.hpp>
 #include <boost/mpl/has_key.hpp>
 
-int main()
-{
-  typedef boost::mpl::set<double,float>    reals;
-  typedef boost::mpl::set<char,short,int>  ints;
+struct _0 { };
+struct _1 { };
+struct _2 { };
+struct _3 { };
+struct _4 { };
+struct _5 { };
+struct _6 { };
 
-  printf("Is int    in reals: %d\n",boost::mpl::has_key<reals,int>::value);
-  printf("Is int    in ints : %d\n",boost::mpl::has_key<ints ,int>::value);
-  printf("Is double in reals: %d\n",boost::mpl::has_key<reals,double>::value);
-  printf("Is double in ints : %d\n",boost::mpl::has_key<ints ,double>::value);
+struct _a { };
+struct _b { };
+struct _c { };
+struct _d { };
+struct _e { };
+struct _f { };
+struct _g { };
+
+int main (void) {
+  typedef boost::mpl::set7<_0, _1, _2, _3, _4, _5, _6> numbers;
+  typedef boost::mpl::set7<_a, _b, _c, _d, _e, _f, _g> letters;
+
+  using namespace boost::mpl;
+
+  if (!has_key<numbers, _2>::value) return 1;
+  if (!has_key<letters, _d>::value) return 1;
+ 
+  if (has_key<numbers, _f>::value) return 1;
+  if (has_key<letters, _0>::value) return 1;
 }
--- test/profiler/nt2_vs_mpl/nt2_has_key.cxx
+++ test/profiler/nt2_vs_mpl/nt2_has_key.cxx
@@ -1,14 +1,31 @@
-#include <cstdio>
 #include <nt2/sdk/meta/set.hpp>
 #include <nt2/sdk/meta/has_key.hpp>
 
-int main()
-{
-  typedef nt2::meta::set<double,float>    reals;
-  typedef nt2::meta::set<char,short,int>  ints;
+struct _0 { };
+struct _1 { };
+struct _2 { };
+struct _3 { };
+struct _4 { };
+struct _5 { };
+struct _6 { };
 
-  printf("Is int    in reals: %d\n",nt2::meta::has_key<reals,int>::value);
-  printf("Is int    in ints : %d\n",nt2::meta::has_key<ints ,int>::value);
-  printf("Is double in reals: %d\n",nt2::meta::has_key<reals,double>::value);
-  printf("Is double in ints : %d\n",nt2::meta::has_key<ints ,double>::value);
+struct _a { };
+struct _b { };
+struct _c { };
+struct _d { };
+struct _e { };
+struct _f { };
+struct _g { };
+
+int main (void) {
+  typedef nt2::meta::set<_0, _1, _2, _3, _4, _5, _6> numbers;
+  typedef nt2::meta::set<_a, _b, _c, _d, _e, _f, _g> letters;
+
+  using namespace nt2::meta;
+
+  if (!has_key<numbers, _2>::value) return 1;
+  if (!has_key<letters, _d>::value) return 1;
+ 
+  if (has_key<numbers, _f>::value) return 1;
+  if (has_key<letters, _0>::value) return 1;
 }
